f6c8ef95097234e2849802f8b237f2c558fff9f7
David Oliver <david.oliver@nventive.com>
2019-10-31 14:22:13 -0400

    [UWP] Fix SamplesApp compile error

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/Button_UseUWPDefaultStyles.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/Button_UseUWPDefaultStyles.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Uno.UI.Sample.Views.Helper;
 using Uno.UI.Samples.Controls;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
